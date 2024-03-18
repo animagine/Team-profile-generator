df2080310e00cc99887594d2fbc1d39c7e3c9a9b
animagine <animagine@live.com>
2024-03-18 22:23:36 +0000

    rendered html

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./src/page-template.js");
-
-
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
 const render = require("./src/page-template.js")
